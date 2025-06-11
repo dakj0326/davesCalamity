60a1c9eb99ab7b6cf06c45304f46400cd39a8df1
davkj006 <davkj006@student.liu.se>
2025-06-11 19:01:49 +0200

    first commit

---
+++ MinecraftMod/src/main/java/net/dave/davesCalamity/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.dave.davesCalamity.item;
+
+import net.dave.davesCalamity.DavesCalamity;
+import net.dave.davesCalamity.item.custom.ChiselItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DavesCalamity.MOD_ID);
+
+    //Items
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite", () -> new Item(new Item.Properties()));
+
+    //Advanced
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
